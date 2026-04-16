b53909331f3d85c21fda48b2eb7052368e2ea223
anasmsdramsees-collab <anasmsdramsees-collab@users.noreply.github.com>
2026-04-16 17:39:19 +0300

    Initial commit: LEOMAX website PWA

---
+++ sw.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'leomax-v1';
+
+const ASSETS = [
+  './LEOMAX_Website_Design.html',
+  './company-about.html',
+  './company-case-studies.html',
+  './company-contact.html',
+  './company-founder.html',
+  './company-process.html',
+  './case-autopoint.html',
+  './case-clenso.html',
+  './case-garage.html',
+  './case-hoolak.html',
+  './case-jambak.html',
+  './case-kbc.html',
+  './case-lanova.html',
+  './case-lsofia.html',
+  './case-nadialamal.html',
+  './case-nourish.html',
+  './case-ramsees.html',
+  './case-redstudio.html',
+  './case-regional.html',
+  './system-01-growth.html',
+  './system-02-ai.html',
+  './system-03-marketing.html',
+  './system-04-content.html',
+  './system-05-launch.html',
+  './manifest.json',
+  './PHOTO-2025-12-12-11-14-59.jpg'
+];
+
+// Install: cache all assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: cache-first strategy
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      });
+    }).catch(() => caches.match('./LEOMAX_Website_Design.html'))
+  );
+});
